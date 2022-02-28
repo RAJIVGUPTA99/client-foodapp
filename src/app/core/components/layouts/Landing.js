6d03b8afaf1ae2634239a1fa1edfdb67656224cd
Rajiv Gupta <54449411+RAJIVGUPTA99@users.noreply.github.com>
2022-02-28 16:08:54 +0530

    Update Landing.js

--- src/app/core/components/layouts/Landing.js
+++ src/app/core/components/layouts/Landing.js
@@ -3,26 +3,28 @@ import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <div className="container-fluid">
-      <div className="row my-5">
-        <div className="col-2" />
-        <div className="col">
-          <h1 className="mb-4 text-center">Food Delivery</h1>
-          <p className="lead text-center">
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
-          </p>
-          <hr />
-          <div className="self-align-center">
-            <Link to="/auth/register" className="btn btn-lg btn-info me-2">
-              Sign Up
-            </Link>
-            <Link to="/auth/login" className="btn btn-lg btn-light">
-              Login
-            </Link>
+    <div className="landing">
+      <div className="container-fluid">
+        <div className="row my-5">
+          <div className="col-2" />
+          <div className="col">
+            <h1 className="mb-4 text">Food Delivery</h1>
+            <p className="lead text">
+              Create a developer profile/portfolio, share posts and get help
+              from other developers
+            </p>
+            <hr />
+            <div className="self-align-center">
+              <Link to="/auth/register" className="btn btn-lg btn-info me-2">
+                Sign Up
+              </Link>
+              <Link to="/auth/login" className="btn btn-lg btn-light">
+                Login
+              </Link>
+            </div>
           </div>
+          <div className="col-2" />
         </div>
-        <div className="col-2" />
       </div>
     </div>
   );
